f86ba617732371b799517f9ded008d0728ea7c56
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-27 14:00:34 -0400

    port

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var port = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function(err, result){
+}).listen(port, 'localhost', function(err, result){
   if(err){
     return console.log(err);
   }
-  console.log("listen on 3000");
+  console.log("listen on ",port);
 });
